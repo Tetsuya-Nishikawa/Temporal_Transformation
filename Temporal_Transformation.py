bec85eae5c919d0f233eb3c3b26045640694cf64
Tetsuya-Nishikawa <te1823ya@gmail.com>
2020-03-07 05:17:44 +0900

    second commit

---
+++ Temporal_Transformation.py
@@ -0,0 +1,60 @@
+import numpy as np
+import skvideo.io
+import glob
+import cv2
+
+
+def ReadVideo(video_name):
+    """
+    ビデオを取得する
+    引数：
+        video_name->ビデオファイル名
+    """
+    return skvideo.io.vread(video_name)
+
+def GetFrames(video_name):
+    """
+    フレーム数を取得する
+    引数:
+        video_name->ビデオファイル名
+    """
+    video = ReadVideo(video_name)
+    return video.shape[0]
+
+def UpSample(video_name, new_frames):
+    """
+    フレーム数を増やす
+    引数：
+        video_name->ビデオファイル名
+        new_frames->新しいフレーム数
+    """
+    now_frames = GetFrames(video_name)
+    video = ReadVideo(video_name)
+    index_list = [int(i) for i in np.linspace(1, now_frames, num=new_frames)]
+
+    return np.array([video[i-1] for i in index_list])
+
+def SaveVideo(video, video_name):
+    """
+    ビデオの保存をする
+    引数：
+        video->ビデオ
+        video_name->ビデオファイル名
+    """
+    fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+    out = cv2.VideoWriter(video_name, 0x7634706d, 20.0, (video.shape[1],video.shape[2]))
+
+    frames = video.shape[0]
+    print("framesは、", frames)
+    for i in range(frames):
+        frame = video[i]
+        print(frame.shape)
+        out.write(frame)
+    out.release()
+
+video_list = glob.glob("/Users/mori/Desktop/all_cut/*.mp4")
+print(ReadVideo(video_list[0]).shape)
+video = UpSample(video_list[0], 100)
+print(video.shape)
+SaveVideo(video, "upsampled_video.mp4")
+SaveVideo(ReadVideo(video_list[0]), "video.mp4")
